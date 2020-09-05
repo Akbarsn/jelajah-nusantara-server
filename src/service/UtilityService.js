2434076a6c755462e015bea37aaadb2168aa82dd
Akbarsn <personal.akbarsn@gmail.com>
2020-09-05 17:42:30 +0700

    fix: Fixing path for public images

--- src/service/UtilityService.js
+++ src/service/UtilityService.js
@@ -2,6 +2,17 @@ const User = require("../model/User");
 const Island = require("../model/Island");
 const Story = require("../model/Story");
 
+function GetBaseURL() {
+  // const env = require("../config").GetEnvVariable();
+
+  // const host = env.Server.Host;
+  // const port = env.Server.Port;
+
+  // return `${host}:${port}`;
+
+  return "https://jelajah-nusantara.herokuapp.com"
+}
+
 module.exports = {
   async MigrateAll() {
     const users = await this.MigrateUser(User);
@@ -45,34 +56,34 @@ module.exports = {
         {
           _id: 1,
           name: "Sumatra",
-          imgSrc: `${process.cwd()}/public/image/sumatra.png`,
+          imgSrc: `${GetBaseURL()}/public/image/sumatra.png`,
           trivias: [
             {
               content:
                 "Mengapa Rumah Gadang  memiliki atapnya yang berbentuk tanduk runcing?bentuk tanduk kerbau tersebut merupakan simbol kemenangan adu kerbau raja Minangkabau melawan kerbau raja di Jawa. Sejak saat itu, tanduk kerbau menjadi penanda kejayaan Minangkabau",
-              imgSrc: `${process.cwd()}/public/image/trivia.png`,
+              imgSrc: `${GetBaseURL()}/public/image/trivia.png`,
             },
             {
               content:
                 "Mengapa Rumah Gadang  memiliki atapnya yang berbentuk tanduk runcing?bentuk tanduk kerbau tersebut merupakan simbol kemenangan adu kerbau raja Minangkabau melawan kerbau raja di Jawa. Sejak saat itu, tanduk kerbau menjadi penanda kejayaan Minangkabau",
-              imgSrc: `${process.cwd()}/public/image/trivia.png`,
+              imgSrc: `${GetBaseURL()}/public/image/trivia.png`,
             },
           ],
         },
         {
           _id: 2,
           name: "Jawa",
-          imgSrc: `${process.cwd()}/public/image/jawa.png`,
+          imgSrc: `${GetBaseURL()}/public/image/jawa.png`,
           trivias: [
             {
               content:
                 "Mengapa Rumah Gadang  memiliki atapnya yang berbentuk tanduk runcing?bentuk tanduk kerbau tersebut merupakan simbol kemenangan adu kerbau raja Minangkabau melawan kerbau raja di Jawa. Sejak saat itu, tanduk kerbau menjadi penanda kejayaan Minangkabau",
-              imgSrc: `${process.cwd()}/public/image/trivia.png`,
+              imgSrc: `${GetBaseURL()}/public/image/trivia.png`,
             },
             {
               content:
                 "Mengapa Rumah Gadang  memiliki atapnya yang berbentuk tanduk runcing?bentuk tanduk kerbau tersebut merupakan simbol kemenangan adu kerbau raja Minangkabau melawan kerbau raja di Jawa. Sejak saat itu, tanduk kerbau menjadi penanda kejayaan Minangkabau",
-              imgSrc: `${process.cwd()}/public/image/trivia.png`,
+              imgSrc: `${GetBaseURL()}/public/image/trivia.png`,
             },
           ],
         },
@@ -92,30 +103,30 @@ module.exports = {
           islandID: 1,
           title: "Atu Belah Atu Bertangkup",
           description: "Cerita Rakyat Dari Provinsi Nangroe Aceh Dasusalam",
-          imgSrc: `${process.cwd()}/public/story/1.png`,
+          imgSrc: `${GetBaseURL()}/public/story/1.png`,
           steps: [
             {
-              imgSrc: `${process.cwd()}/public/story/1-1.png`,
+              imgSrc: `${GetBaseURL()}/public/story/1-1.png`,
               content:
                 "Dahulu kala, hiduplah sepasang suami isteri yang sangat miskin. Suami isteri itu memiliki dua anak laki laki yang masih kecil. Kehidupan keluarga itu sangt prihatin. setiap hari suami bekerja di ladang",
             },
             {
-              imgSrc: `${process.cwd()}/public/story/1-2.png`,
+              imgSrc: `${GetBaseURL()}/public/story/1-2.png`,
               content:
                 "Jika hasil ladang tidak begitu baik, sisuami berburu di hutan. apabila beruntung, si suami akan mendapatkan seekor rusa yang gemuk. ",
             },
             {
-              imgSrc: `${process.cwd()}/public/story/1-3.png`,
+              imgSrc: `${GetBaseURL()}/public/story/1-3.png`,
               content:
                 "Namun, sering kali rusa sulit didapatkan. Jika demikian hanya belalang yang bisa ditangkapnya Belalang - belalang yang ditangkap, di simpan di lumbung agar tidak beterbangan keluar",
             },
             {
-              imgSrc: `${process.cwd()}/public/story/1-4.png`,
+              imgSrc: `${GetBaseURL()}/public/story/1-4.png`,
               content:
                 "Pagi itu si suami berkata “Hari ini aku akan berburu kijang. tunggulah sampai siang, kita akan makan daging rusa sepuasnya!” si suami juga berpesan kalau masuk kelumbung jangan lupa menutup pintunya agar belalang-belalang itu tidak kabur.",
             },
             {
-              imgSrc: `${process.cwd()}/public/story/1-5.png`,
+              imgSrc: `${GetBaseURL()}/public/story/1-5.png`,
               content:
                 "Si suami segera masuk kehutan dengan harapan mendapatkan seekor kijang yang gemuk. Istri dan anak anaknya tentunya sangat bahagia jika makan daging rusa yang lezat",
             },
