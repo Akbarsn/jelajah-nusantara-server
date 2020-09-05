5a16f475670fede55e39ee06c70d9b1cd73a401c
Akbarsn <personal.akbarsn@gmail.com>
2020-09-05 17:13:24 +0700

    feat: Add get island detail route

--- src/route/HomepageRoute.js
+++ src/route/HomepageRoute.js
@@ -1,4 +1,5 @@
 const Island = require("../model/Island");
+const Story = require("../model/Story");
 const HomepageService = require("../service/HomepageService");
 
 async function GetAllIslandRoute(req, res, next) {
@@ -14,8 +15,29 @@ async function GetAllIslandRoute(req, res, next) {
   }
 }
 
+async function GetIslandDetailRoute(req, res, next) {
+  try {
+    const IslandID = req.params.islandID;
+
+    const detail = await HomepageService.GetIslandDetail(
+      Island,
+      Story,
+      IslandID
+    );
+
+    if (detail.status) {
+      res.status(200).json(detail.data);
+    } else {
+      throw detail.error;
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
 function HomepageRoute(router) {
   router.get("/", GetAllIslandRoute);
+  router.get("/detail/:islandID", GetIslandDetailRoute);
 
   return router;
 }
--- src/service/HomepageService.js
+++ src/service/HomepageService.js
@@ -1,4 +1,4 @@
-const { GeneralError } = require("../utils/Error");
+const { GeneralError, BadRequest } = require("../utils/Error");
 
 module.exports = {
   async GetAllIsland(IslandModel) {
@@ -6,6 +6,34 @@ module.exports = {
       const islands = await IslandModel.find({});
 
       return { status: true, data: islands };
+    } catch (error) {
+      console.log("Here");
+      console.log(error);
+      return { status: false, error: error };
+    }
+  },
+
+  async GetIslandDetail(IslandModel, StoryModel, IslandID) {
+    try {
+      const island = await IslandModel.findOne({
+        _id: IslandID,
+      });
+
+      if (island) {
+        const stories = await StoryModel.find({
+          islandID: IslandID,
+        });
+
+        const response = {
+          island,
+          stories: stories,
+        };
+        console.log(response);
+
+        return { status: true, data: response };
+      } else {
+        return { status: false, error: new BadRequest("Island ID is wrong") };
+      }
     } catch (error) {
       return { status: false, error: new GeneralError("Query failed") };
     }
